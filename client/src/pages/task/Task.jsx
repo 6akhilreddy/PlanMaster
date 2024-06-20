1761d295532b1e44df27224f6e0f8200f7a5608b
a0p0ic5 <a0p0ic5@homeoffice.wal-mart.com>
2024-06-20 17:39:40 +0530

    redeploy

--- client/src/pages/task/Task.jsx
+++ client/src/pages/task/Task.jsx
@@ -1,7 +1,7 @@
 const Task = () => {
   return (
     <p className='flex min-h-[30rem] justify-center items-center text-xl mx-auto w-full font-semibold md:text-2xl'>
-      Coming soon
+      Coming soon..
     </p>
   );
 };
